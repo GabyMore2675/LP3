795a6cfeea0693ede02bfef46c9fba4f1d79d50a
Gabriela2675 <130612014+Gabriela2675@users.noreply.github.com>
2023-08-23 16:23:32 -0500

    Programa secundario

--- PyLab01/src/PyLab01/Principal.java
+++ PyLab01/src/PyLab01/Principal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Principal {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		//comentario
 		int vida, rp;
 		boolean fl = false;
 		vida = 0;
---
+++ PyLab01/src/PyLab01/Secundario.java
@@ -0,0 +1,8 @@
+package PyLab01;
+
+public class Secundario {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo...");
+	}
+}
