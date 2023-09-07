b555e7f479b1ed74d1106fba0c852e4f37ed3e97
GabyMore2675 <142509195+GabyMore2675@users.noreply.github.com>
2023-09-07 07:19:27 -0600

    Update and rename ContadorTest2 to ContadorTest2.java

---
+++ LP3_lab03/PyContador/src/PyContador/ContadorTest2.java
@@ -0,0 +1,9 @@
+package PyContador;
+
+public class ContadorTest2 {
+	public static void main(String[] args) {
+		System.out.println("VALOR_INICIAL");
+		System.out.println(Contador.VALOR_INICIAL);
+	}
+}
+
