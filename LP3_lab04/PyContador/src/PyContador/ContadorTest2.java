f52a06fd0e0cb51e3fb39d09bbb8a123d1a36473
GabyMore2675 <142509195+GabyMore2675@users.noreply.github.com>
2023-09-07 07:46:10 -0600

    Delete LP3_lab04/PyContador directory

--- LP3_lab04/PyContador/src/PyContador/ContadorTest2.java
+++
@@ -1,8 +0,0 @@
-package PyContador;
-
-public class ContadorTest2 {
-	public static void main(String[] args) {
-		System.out.println("VALOR_INICIAL");
-		System.out.println(Contador.VALOR_INICIAL);
-	}
-}
