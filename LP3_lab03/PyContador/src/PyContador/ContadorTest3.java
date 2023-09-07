5c00105ed698d8a4809eb2badd2b597441abff82
GabyMore2675 <142509195+GabyMore2675@users.noreply.github.com>
2023-09-07 07:19:56 -0600

    Create ContadorTest3.java

---
+++ LP3_lab03/PyContador/src/PyContador/ContadorTest3.java
@@ -0,0 +1,18 @@
+package PyContador;
+
+public class ContadorTest3 {
+    public static void main(String[] args) {
+        
+        Contador c1 = new Contador(5);
+        Contador c2 = new Contador();
+
+        c1.inc(); 
+        c2.inc(); 
+
+        System.out.println("Valor contador1: " + c1.getValor());
+        System.out.println("Valor contador2: " + c2.getValor());
+        System.out.println("Acumulador: " + Contador.acumulador());
+        System.out.println("Numero de contadores creados: " + Contador.nContadores);
+        System.out.println("Ultimo valor inicial del contador creado: " + Contador.ultimoContador);
+    }
+}
